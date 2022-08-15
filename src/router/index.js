ea7b570a6f1c97eeb52574ae7ba9c0873add289c
Aria Bagaswara <ariabagaswara71@gmail.com>
2022-08-15 16:31:41 +0700

    ADD: New Page ''Menu"

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [{
-    path: '/',
-    name: 'home',
-    component: () =>
-        import( /* webpackChunkName: "home" */ '../views/Home.vue')
+  path: '/',
+  name: 'home',
+  component: () =>
+    import('../views/Home.vue')
+},
+{
+  path: '/menu',
+  name: 'menu',
+  component: () =>
+    import('../views/Menu.vue')
 }
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(),
+  routes
 })
 
 export default router
---
+++ src/views/Menu.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="menu">
+    <div class="container mt-15 mb-15">
+      <div class="food-menu-wrapper">
+        <h1 class="content-title">Menu</h1>
+        <p class="content-sub-title">Explore foods you want</p>
+        <div class="food-menu-list grid">
+          <div class="food-menu-item" v-for="i in 16" :key="i">
+            <img class="food-img" src="../assets/hero.jpg" alt="">
+            <div class="food-content">
+              <h2 class="food-title">Ayam Geprek</h2>
+              <p class="food-description">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <button class="add-to-cart-button">
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Menu',
+}
+</script>
+<style lang="scss" scoped>
+</style>
